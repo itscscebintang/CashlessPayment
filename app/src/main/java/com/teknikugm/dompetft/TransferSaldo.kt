32e8d47773a9b036652d755c053e705411ea5a56
itscscebintang <bintang10janshavira@gmail.com>
2021-02-11 17:19:52 +0700

    commint tiga, bikin tampilan top up 2

---
+++ app/src/main/java/com/teknikugm/dompetft/TransferSaldo.kt
@@ -0,0 +1,11 @@
+package com.teknikugm.dompetft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransferSaldo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer_saldo)
+    }
+}
