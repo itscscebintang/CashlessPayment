f4c2413576bb25fcb4b57af39868a7e1fb4bd89f
itscscebintang <bintang10janshavira@gmail.com>
2021-02-12 22:35:09 +0700

    commint empat

---
+++ app/src/main/java/com/teknikugm/dompetft/Pay_Canteen_QR.kt
@@ -0,0 +1,11 @@
+package com.teknikugm.dompetft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pay_Canteen_QR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay__canteen__q_r)
+    }
+}
